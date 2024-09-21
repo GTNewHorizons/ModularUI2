c85158e34d1accf5cffdec5565aca78f9b9b154b
Andrey <timeconqueror999@gmail.com>
2024-09-21 16:16:32 +0300

    fix: ClientTick event was not called for screens (#16)

--- src/main/java/com/cleanroommc/modularui/ClientProxy.java
+++ src/main/java/com/cleanroommc/modularui/ClientProxy.java
@@ -10,19 +10,20 @@ import com.cleanroommc.modularui.test.EventHandler;
 import com.cleanroommc.modularui.test.OverlayTest;
 import com.cleanroommc.modularui.theme.ThemeManager;
 import com.cleanroommc.modularui.theme.ThemeReloadCommand;
-import cpw.mods.fml.client.registry.RenderingRegistry;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Optional;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.IReloadableResourceManager;
 import net.minecraft.util.Timer;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
+
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 @SuppressWarnings("unused")
@@ -35,7 +36,11 @@ public class ClientProxy extends CommonProxy {
         super.preInit(event);
 
         FMLCommonHandler.instance().bus().register(new ClientEventHandler());
-        MinecraftForge.EVENT_BUS.register(new ClientScreenHandler());
+
+        ClientScreenHandler clientScreenHandler = new ClientScreenHandler();
+        // registered to both buses since handled events are not bound to a single bus
+        FMLCommonHandler.instance().bus().register(clientScreenHandler);
+        MinecraftForge.EVENT_BUS.register(clientScreenHandler);
         MinecraftForge.EVENT_BUS.register(new OverlayManager());
 
         if (ModularUIConfig.enableTestGuis) {
