9bfbbcb0f140ed01f272b2a553bd6d7718f6c8ef
miozune <miozune@gmail.com>
2024-12-12 13:00:50 -0500

    Textfield improvements (#81)
    * Allow focusing on text field upon GUI open
    
    * Remove selected chars on key type or paste
    
    Handler test prevents invalid chars from removing textbox
    
    * Prevent § from being pasted
    
    * Shorten cursor blink rate to 0.5s
    
    This is the same behavior as of MUI1. Also Windows uses 530 ms by default.
    
    * Parse thousand separator
    
    * Clear text on right click
    
    * Add support for suffixes and E notation
    
    The implementation is somewhat dirty, but I can't think of better way.
    I renamed constant E to NAPIER, as it clashes with E notation. I don't think someone will use this anyway.
    
    * Allow retrieving parse fail message
    
    Printing to log might make people think it's bug in code.
    This feature can be used for indicator of what's wrong with the use input.
    This also stops function construction from failing silently.
    
    * Allow setting default number
    
    * Make division by zero throw exception
    
    I believe this is better than silently dividing by 1.
    
    * Add config to restore the last text if ESC key is pressed
    
    * Handle cases failed to parse function
    
    * Catch all the exceptions
    
    It's not game-breaking fatal error anyway.
    
    * Split into text field and numeric field
    
    Former defaultValue is gone, because 1. It's set by onInit 2. It's better to restore the last value rather than something supposed to be default
    
    * Rename
    
    TextFieldWidget -> OneLineTextField
    StringTextFieldWidget -> TextFieldWidget
    
    * Dirty solution for comma as thousand separator
    
    * Rewrite the way E notation is handled
    
    * Revert "Rename"
    
    This reverts commit 8e00147da9ecedff50430fba71ec94fec6f8b920.
    
    * Revert "Split into text field and numeric field"
    
    This reverts commit 580683bcc4af9419be69d6e975a8834c0ee81878.
    
    (cherry picked from commit 53f305ecb8251dada4fa2876a71be153c0d2d162)

--- src/main/java/com/cleanroommc/modularui/widgets/textfield/TextFieldHandler.java
+++ src/main/java/com/cleanroommc/modularui/widgets/textfield/TextFieldHandler.java
@@ -323,6 +323,11 @@ public class TextFieldHandler {
         setCursor(this.cursor.y + 1, 0, false);
     }
 
+    public void clear() {
+        markAll();
+        delete();
+    }
+
     public void delete() {
         delete(false);
     }
