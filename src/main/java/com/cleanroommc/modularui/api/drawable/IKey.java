CleanroomMC/ModularUI

refactor formatting stuff

src/main/java/com/cleanroommc/modularui/api/drawable/IKey.java
@@ -167,7 +167,7 @@ public interface IKey extends IDrawable {
      * @param parentFormatting formatting of the parent in case of composite keys
      * @return the current formatted string
      */
-    default String getFormatted(@Nullable TextFormatting[] parentFormatting) {
+    default String getFormatted(@Nullable FormattingState parentFormatting) {
         return get();
     }
 
@@ -202,12 +202,32 @@ public interface IKey extends IDrawable {
         return new AnimatedText(this);
     }
 
-    IKey format(TextFormatting formatting);
+    /**
+     * @return a formatting state of this key
+     */
+    default @Nullable FormattingState getFormatting() {
+        return null;
+    }
 
-    default IKey format(TextFormatting... formatting) {
-        for (TextFormatting tf : formatting) format(tf);
+    /**
+     * Set text formatting to this key. If {@link IKey#RESET} is used, then that's applied first and then all other formatting of this key.
+     * With {@link null}, you can remove a color formatting. No matter the parents color, the default color will be used.
+     *
+     * @param formatting a formatting rul
+     * @return this
+     */
+    IKey style(@Nullable TextFormatting formatting);
+
+    default IKey style(TextFormatting... formatting) {
+        for (TextFormatting tf : formatting) style(tf);
         return this;
     }
+
+    default IKey removeFormatColor() {
+        return style((TextFormatting) null);
+    }
+
+    IKey removeStyle();
 
     default StyledText alignment(Alignment alignment) {
         return withStyle().alignment(alignment);

src/main/java/com/cleanroommc/modularui/drawable/text/AnimatedText.java
@@ -103,8 +103,8 @@ public class AnimatedText extends StyledText {
     }
 
     @Override
-    public AnimatedText format(TextFormatting formatting) {
-        return (AnimatedText) super.format(formatting);
+    public AnimatedText style(TextFormatting formatting) {
+        return (AnimatedText) super.style(formatting);
     }
 
     @Override

src/main/java/com/cleanroommc/modularui/drawable/text/BaseKey.java
@@ -9,24 +9,33 @@ import org.jetbrains.annotations.Nullable;
 
 public abstract class BaseKey implements IKey {
 
-    private TextFormatting[] formatting;
+    private FormattingState formatting;
 
     @Override
-    public String getFormatted(@Nullable TextFormatting[] parentFormatting) {
+    public String getFormatted(@Nullable FormattingState parentFormatting) {
         return FontRenderHelper.format(this.formatting, parentFormatting, get());
     }
 
     @Override
-    public BaseKey format(TextFormatting formatting) {
+    public BaseKey style(@Nullable TextFormatting formatting) {
         if (this.formatting == null) {
-            this.formatting = FontRenderHelper.createFormattingState();
+            this.formatting = new FormattingState();
         }
-        FontRenderHelper.addAfter(this.formatting, formatting);
+        if (formatting == null) this.formatting.forceDefaultColor();
+        else this.formatting.add(formatting, false);
         return this;
     }
 
-    @Nullable
-    public TextFormatting[] getFormatting() {
+    @Override
+    public IKey removeStyle() {
+        if (this.formatting != null) {
+            this.formatting.reset();
+        }
+        return this;
+    }
+
+    @Override
+    public @Nullable FormattingState getFormatting() {
         return formatting;
     }
 

src/main/java/com/cleanroommc/modularui/drawable/text/FontRenderHelper.java
@@ -1,12 +1,11 @@
 package com.cleanroommc.modularui.drawable.text;
 
 import com.cleanroommc.modularui.api.MCHelper;
+import com.cleanroommc.modularui.api.drawable.IKey;
 
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.util.text.TextFormatting;
 
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
@@ -39,20 +38,14 @@ public class FontRenderHelper {
         return formattingMap[c - min];
     }
 
-    // a formatting state keeps track of a color format and each of the fancy style options
-    // 0: color, 1 - 5: fancy style (random, bolt, italic, underline, strikethrough), 6: reset
-    public static TextFormatting[] createFormattingState() {
-        return new TextFormatting[7];
-    }
-
-    public static void addAfter(TextFormatting[] state, TextFormatting formatting) {
+    public static void addAfter(TextFormatting[] state, TextFormatting formatting, boolean removeAllOnReset) {
         if (formatting == TextFormatting.RESET) {
-            Arrays.fill(state, null);
-            state[6] = formatting;
+            if (removeAllOnReset) Arrays.fill(state, null);
+            state[0] = formatting;
             return;
         }
         // remove reset
-        state[6] = null;
+        if (removeAllOnReset) state[6] = null;
         if (formatting.isFancyStyling()) {
             state[formatting.ordinal() - 15] = formatting;
             return;
@@ -61,67 +54,27 @@ public class FontRenderHelper {
         state[0] = formatting;
     }
 
-    public static void parseFormattingState(TextFormatting[] state, String text) {
-        int i = -2;
-        while ((i = text.indexOf(167, i + 2)) >= 0 && i < text.length() - 1) {
-            TextFormatting formatting = getForCharacter(text.charAt(i + 1));
-            if (formatting != null) addAfter(state, formatting);
-        }
-    }
-
-    public static String getFormatting(TextFormatting[] state) {
-        if (isReset(state)) return TextFormatting.RESET.toString();
-        StringBuilder builder = appendFormatting(state, new StringBuilder());
-        return builder.length() == 0 ? StringUtils.EMPTY : builder.toString();
-    }
-
-    public static StringBuilder appendFormatting(TextFormatting[] state, StringBuilder builder) {
-        return appendFormatting(state, null, builder);
-    }
-
-    public static StringBuilder appendFormatting(TextFormatting[] state, TextFormatting @Nullable [] fallback, StringBuilder builder) {
-        for (int i = 0, n = 6; i < n; i++) {
-            if (state[i] != null) {
-                builder.append(state[i]);
-            } else if (fallback != null && fallback[i] != null) {
-                builder.append(fallback[i]);
-            }
-        }
-        return builder;
-    }
-
-    public static String format(@Nullable TextFormatting[] state, @Nullable TextFormatting[] parentState, String text) {
+    public static String format(@Nullable FormattingState state, @Nullable FormattingState parentState, String text) {
         if (state == null) {
             if (parentState == null) return text;
-            return appendFormatting(parentState, new StringBuilder().append(TextFormatting.RESET)).append(text).toString();
+            return parentState.prependText(new StringBuilder().append(TextFormatting.RESET)).append(text).toString();
         }
-        StringBuilder s = appendFormatting(state, parentState, new StringBuilder().append(TextFormatting.RESET))
+        StringBuilder s = state.prependText(new StringBuilder().append(TextFormatting.RESET), parentState)
                 .append(text);
         return s.toString();
     }
 
-    public static TextFormatting @NotNull [] mergeState(TextFormatting @Nullable [] state1, TextFormatting @Nullable [] state2) {
-        return mergeState(state1, state2, null);
-    }
-
-    public static TextFormatting @NotNull [] mergeState(TextFormatting @Nullable [] state1, TextFormatting @Nullable [] state2, TextFormatting @Nullable [] result) {
-        if (state1 == null) {
-            if (state2 == null) return createFormattingState();
-            return state2;
-        } else if (state2 == null) {
-            return state1;
+    public static String formatArgs(Object[] args, @Nullable FormattingState parentState, String text) {
+        if (args == null || args.length == 0) return text;
+        args = Arrays.copyOf(args, args.length);
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof IKey key) {
+                // parent format + key format + key text + parent format
+                args[i] = FormattingState.appendFormat(new StringBuilder(key.getFormatted(parentState))
+                        .append(TextFormatting.RESET), parentState).toString();
+            }
         }
-        if (isReset(state2)) return state2; // state2 has higher priority
-        if (result == null) result = Arrays.copyOf(state1, state1.length);
-        for (int i = 0, n = 6; i < n; i++) {
-            TextFormatting formatting = state2[i];
-            if (formatting != null) addAfter(result, formatting);
-        }
-        return result;
-    }
-
-    public static boolean isReset(TextFormatting[] state) {
-        return state[6] != null;
+        return String.format(text, args);
     }
 
     public static int getDefaultTextHeight() {

src/main/java/com/cleanroommc/modularui/drawable/text/StyledText.java
@@ -62,8 +62,8 @@ public class StyledText extends BaseKey {
     }
 
     @Override
-    public BaseKey format(TextFormatting formatting) {
-        this.key.format(formatting);
+    public BaseKey style(TextFormatting formatting) {
+        this.key.style(formatting);
         return this;
     }
 

src/main/java/com/cleanroommc/modularui/widgets/TextWidget.java
@@ -113,8 +113,8 @@ public class TextWidget extends Widget<TextWidget> {
         return this;
     }
 
-    public TextWidget format(TextFormatting formatting) {
-        this.key.format(formatting);
+    public TextWidget style(TextFormatting formatting) {
+        this.key.style(formatting);
         return this;
     }
 }

GTNewHorizons/ModularUI2

(cherry picked from commit 134a9756a5c66582f60783f9e71bc47f380012ba)

src/main/java/com/cleanroommc/modularui/api/drawable/IKey.java
@@ -167,7 +167,7 @@ public interface IKey extends IDrawable {
      * @param parentFormatting formatting of the parent in case of composite keys
      * @return the current formatted string
      */
-    default String getFormatted(@Nullable EnumChatFormatting[] parentFormatting) {
+    default String getFormatted(@Nullable FormattingState parentFormatting) {
         return get();
     }
 
@@ -202,12 +202,32 @@ public interface IKey extends IDrawable {
         return new AnimatedText(this);
     }
 
-    IKey format(EnumChatFormatting formatting);
+    /**
+     * @return a formatting state of this key
+     */
+    default @Nullable FormattingState getFormatting() {
+        return null;
+    }
 
-    default IKey format(EnumChatFormatting... formatting) {
-        for (EnumChatFormatting tf : formatting) format(tf);
+    /**
+     * Set text formatting to this key. If {@link IKey#RESET} is used, then that's applied first and then all other formatting of this key.
+     * With {@link null}, you can remove a color formatting. No matter the parents color, the default color will be used.
+     *
+     * @param formatting a formatting rul
+     * @return this
+     */
+    IKey style(@Nullable EnumChatFormatting formatting);
+
+    default IKey style(EnumChatFormatting... formatting) {
+        for (EnumChatFormatting tf : formatting) style(tf);
         return this;
     }
+
+    default IKey removeFormatColor() {
+        return style((EnumChatFormatting) null);
+    }
+
+    IKey removeStyle();
 
     default StyledText alignment(Alignment alignment) {
         return withStyle().alignment(alignment);

src/main/java/com/cleanroommc/modularui/drawable/text/AnimatedText.java
@@ -4,10 +4,11 @@ import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.screen.viewport.GuiContext;
 import com.cleanroommc.modularui.theme.WidgetTheme;
 import com.cleanroommc.modularui.utils.Alignment;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.EnumChatFormatting;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -102,8 +103,8 @@ public class AnimatedText extends StyledText {
     }
 
     @Override
-    public AnimatedText format(EnumChatFormatting formatting) {
-        return (AnimatedText) super.format(formatting);
+    public AnimatedText style(EnumChatFormatting formatting) {
+        return (AnimatedText) super.style(formatting);
     }
 
     @Override

src/main/java/com/cleanroommc/modularui/drawable/text/BaseKey.java
@@ -9,24 +9,33 @@ import org.jetbrains.annotations.Nullable;
 
 public abstract class BaseKey implements IKey {
 
-    private EnumChatFormatting[] formatting;
+    private FormattingState formatting;
 
     @Override
-    public String getFormatted(@Nullable EnumChatFormatting[] parentFormatting) {
+    public String getFormatted(@Nullable FormattingState parentFormatting) {
         return FontRenderHelper.format(this.formatting, parentFormatting, get());
     }
 
     @Override
-    public BaseKey format(EnumChatFormatting formatting) {
+    public BaseKey style(@Nullable EnumChatFormatting formatting) {
         if (this.formatting == null) {
-            this.formatting = FontRenderHelper.createFormattingState();
+            this.formatting = new FormattingState();
         }
-        FontRenderHelper.addAfter(this.formatting, formatting);
+        if (formatting == null) this.formatting.forceDefaultColor();
+        else this.formatting.add(formatting, false);
         return this;
     }
 
-    @Nullable
-    public EnumChatFormatting[] getFormatting() {
+    @Override
+    public IKey removeStyle() {
+        if (this.formatting != null) {
+            this.formatting.reset();
+        }
+        return this;
+    }
+
+    @Override
+    public @Nullable FormattingState getFormatting() {
         return formatting;
     }
 

src/main/java/com/cleanroommc/modularui/drawable/text/FontRenderHelper.java
@@ -1,12 +1,11 @@
 package com.cleanroommc.modularui.drawable.text;
 
 import com.cleanroommc.modularui.api.MCHelper;
+import com.cleanroommc.modularui.api.drawable.IKey;
 
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.util.EnumChatFormatting;
 
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
@@ -39,20 +38,14 @@ public class FontRenderHelper {
         return formattingMap[c - min];
     }
 
-    // a formatting state keeps track of a color format and each of the fancy style options
-    // 0: color, 1 - 5: fancy style (random, bolt, italic, underline, strikethrough), 6: reset
-    public static EnumChatFormatting[] createFormattingState() {
-        return new EnumChatFormatting[7];
-    }
-
-    public static void addAfter(EnumChatFormatting[] state, EnumChatFormatting formatting) {
+    public static void addAfter(EnumChatFormatting[] state, EnumChatFormatting formatting, boolean removeAllOnReset) {
         if (formatting == EnumChatFormatting.RESET) {
-            Arrays.fill(state, null);
-            state[6] = formatting;
+            if (removeAllOnReset) Arrays.fill(state, null);
+            state[0] = formatting;
             return;
         }
         // remove reset
-        state[6] = null;
+        if (removeAllOnReset) state[6] = null;
         if (formatting.isFancyStyling()) {
             state[formatting.ordinal() - 15] = formatting;
             return;
@@ -61,67 +54,27 @@ public class FontRenderHelper {
         state[0] = formatting;
     }
 
-    public static void parseFormattingState(EnumChatFormatting[] state, String text) {
-        int i = -2;
-        while ((i = text.indexOf(167, i + 2)) >= 0 && i < text.length() - 1) {
-            EnumChatFormatting formatting = getForCharacter(text.charAt(i + 1));
-            if (formatting != null) addAfter(state, formatting);
-        }
-    }
-
-    public static String getFormatting(EnumChatFormatting[] state) {
-        if (isReset(state)) return EnumChatFormatting.RESET.toString();
-        StringBuilder builder = appendFormatting(state, new StringBuilder());
-        return builder.length() == 0 ? StringUtils.EMPTY : builder.toString();
-    }
-
-    public static StringBuilder appendFormatting(EnumChatFormatting[] state, StringBuilder builder) {
-        return appendFormatting(state, null, builder);
-    }
-
-    public static StringBuilder appendFormatting(EnumChatFormatting[] state, EnumChatFormatting @Nullable [] fallback, StringBuilder builder) {
-        for (int i = 0, n = 6; i < n; i++) {
-            if (state[i] != null) {
-                builder.append(state[i]);
-            } else if (fallback != null && fallback[i] != null) {
-                builder.append(fallback[i]);
-            }
-        }
-        return builder;
-    }
-
-    public static String format(@Nullable EnumChatFormatting[] state, @Nullable EnumChatFormatting[] parentState, String text) {
+    public static String format(@Nullable FormattingState state, @Nullable FormattingState parentState, String text) {
         if (state == null) {
             if (parentState == null) return text;
-            return appendFormatting(parentState, new StringBuilder().append(EnumChatFormatting.RESET)).append(text).toString();
+            return parentState.prependText(new StringBuilder().append(EnumChatFormatting.RESET)).append(text).toString();
         }
-        StringBuilder s = appendFormatting(state, parentState, new StringBuilder().append(EnumChatFormatting.RESET))
+        StringBuilder s = state.prependText(new StringBuilder().append(EnumChatFormatting.RESET), parentState)
                 .append(text);
         return s.toString();
     }
 
-    public static EnumChatFormatting @NotNull [] mergeState(EnumChatFormatting @Nullable [] state1, EnumChatFormatting @Nullable [] state2) {
-        return mergeState(state1, state2, null);
-    }
-
-    public static EnumChatFormatting @NotNull [] mergeState(EnumChatFormatting @Nullable [] state1, EnumChatFormatting @Nullable [] state2, EnumChatFormatting @Nullable [] result) {
-        if (state1 == null) {
-            if (state2 == null) return createFormattingState();
-            return state2;
-        } else if (state2 == null) {
-            return state1;
+    public static String formatArgs(Object[] args, @Nullable FormattingState parentState, String text) {
+        if (args == null || args.length == 0) return text;
+        args = Arrays.copyOf(args, args.length);
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof IKey key) {
+                // parent format + key format + key text + parent format
+                args[i] = FormattingState.appendFormat(new StringBuilder(key.getFormatted(parentState))
+                        .append(EnumChatFormatting.RESET), parentState).toString();
+            }
         }
-        if (isReset(state2)) return state2; // state2 has higher priority
-        if (result == null) result = Arrays.copyOf(state1, state1.length);
-        for (int i = 0, n = 6; i < n; i++) {
-            EnumChatFormatting formatting = state2[i];
-            if (formatting != null) addAfter(result, formatting);
-        }
-        return result;
-    }
-
-    public static boolean isReset(EnumChatFormatting[] state) {
-        return state[6] != null;
+        return String.format(text, args);
     }
 
     public static int getDefaultTextHeight() {

src/main/java/com/cleanroommc/modularui/drawable/text/StyledText.java
@@ -62,8 +62,8 @@ public class StyledText extends BaseKey {
     }
 
     @Override
-    public BaseKey format(EnumChatFormatting formatting) {
-        this.key.format(formatting);
+    public BaseKey style(EnumChatFormatting formatting) {
+        this.key.style(formatting);
         return this;
     }
 

src/main/java/com/cleanroommc/modularui/widgets/TextWidget.java
@@ -118,8 +118,8 @@ public class TextWidget extends Widget<TextWidget> {
         return this;
     }
 
-    public TextWidget format(EnumChatFormatting formatting) {
-        this.key.format(formatting);
+    public TextWidget style(EnumChatFormatting formatting) {
+        this.key.style(formatting);
         return this;
     }
 }