CleanroomMC/ModularUI

Refactor Container customiztion and phantom slots & crafting grids (#118)
* separate phantom slots from vanilla slots

* UISettings, extendable ModularContainer, remove ContainerCustomizer and more

* reimplement shift transfer to phantom slots

* use final static vars for sync ids

* easy crafting grids & player inv improvements

* fix

* fix

src/main/java/com/cleanroommc/modularui/factory/ClientGUI.java
@@ -1,9 +1,10 @@
 package com.cleanroommc.modularui.factory;
 
 import com.cleanroommc.modularui.api.MCHelper;
-import com.cleanroommc.modularui.screen.ContainerCustomizer;
 import com.cleanroommc.modularui.screen.JeiSettingsImpl;
+import com.cleanroommc.modularui.screen.ModularContainer;
 import com.cleanroommc.modularui.screen.ModularScreen;
+import com.cleanroommc.modularui.screen.UISettings;
 
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.relauncher.Side;
@@ -11,6 +12,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
 
 /**
  * Helper class to open client only GUIs. This class is safe to use inside a Modular GUI.
@@ -29,7 +32,7 @@ public class ClientGUI {
      * @param screen new modular screen
      */
     public static void open(@NotNull ModularScreen screen) {
-        open(screen, new JeiSettingsImpl(), null);
+        open(screen, new UISettings());
     }
 
     /**
@@ -40,30 +43,45 @@ public class ClientGUI {
      * @param jeiSettings custom jei settings
      */
     public static void open(@NotNull ModularScreen screen, @NotNull JeiSettingsImpl jeiSettings) {
-        GuiManager.openScreen(screen, jeiSettings, null);
+        GuiManager.openScreen(screen, new UISettings(jeiSettings));
     }
 
     /**
      * Opens a modular screen on the next client tick with custom jei settings.
      * It needs to be opened in next tick, because we might break the current GUI if we open it now.
      *
-     * @param screen              new modular screen
-     * @param containerCustomizer container customizer
+     * @param screen    new modular screen
+     * @param container custom container
      */
-    public static void open(@NotNull ModularScreen screen, @NotNull ContainerCustomizer containerCustomizer) {
-        GuiManager.openScreen(screen, new JeiSettingsImpl(), containerCustomizer);
+    public static void open(@NotNull ModularScreen screen, @Nullable Supplier<ModularContainer> container) {
+        UISettings settings = new UISettings();
+        settings.customContainer(container);
+        GuiManager.openScreen(screen, settings);
     }
 
     /**
      * Opens a modular screen on the next client tick with custom jei settings.
      * It needs to be opened in next tick, because we might break the current GUI if we open it now.
      *
-     * @param screen              new modular screen
-     * @param jeiSettings         custom jei settings
-     * @param containerCustomizer container customizer
+     * @param screen      new modular screen
+     * @param jeiSettings custom jei settings
+     * @param container   custom container
      */
-    public static void open(@NotNull ModularScreen screen, @NotNull JeiSettingsImpl jeiSettings, @Nullable ContainerCustomizer containerCustomizer) {
-        GuiManager.openScreen(screen, jeiSettings, containerCustomizer);
+    public static void open(@NotNull ModularScreen screen, @NotNull JeiSettingsImpl jeiSettings, @Nullable Supplier<ModularContainer> container) {
+        UISettings settings = new UISettings(jeiSettings);
+        settings.customContainer(container);
+        GuiManager.openScreen(screen, settings);
+    }
+
+    /**
+     * Opens a modular screen on the next client tick with custom jei settings.
+     * It needs to be opened in next tick, because we might break the current GUI if we open it now.
+     *
+     * @param screen   new modular screen
+     * @param settings ui settings
+     */
+    public static void open(@NotNull ModularScreen screen, @NotNull UISettings settings) {
+        GuiManager.openScreen(screen, settings);
     }
 
     /**

src/main/java/com/cleanroommc/modularui/widgets/slot/ItemSlot.java
@@ -1,6 +1,5 @@
-package com.cleanroommc.modularui.widgets;
+package com.cleanroommc.modularui.widgets.slot;
 
-import com.cleanroommc.modularui.ModularUI;
 import com.cleanroommc.modularui.api.ITheme;
 import com.cleanroommc.modularui.api.widget.IVanillaSlot;
 import com.cleanroommc.modularui.api.widget.Interactable;
@@ -9,23 +8,18 @@ import com.cleanroommc.modularui.core.mixin.GuiContainerAccessor;
 import com.cleanroommc.modularui.core.mixin.GuiScreenAccessor;
 import com.cleanroommc.modularui.drawable.GuiDraw;
 import com.cleanroommc.modularui.drawable.text.TextRenderer;
-import com.cleanroommc.modularui.integration.jei.JeiGhostIngredientSlot;
 import com.cleanroommc.modularui.integration.jei.JeiIngredientProvider;
-import com.cleanroommc.modularui.integration.jei.ModularUIJeiPlugin;
 import com.cleanroommc.modularui.screen.ClientScreenHandler;
-import com.cleanroommc.modularui.screen.ModularScreen;
 import com.cleanroommc.modularui.screen.RichTooltip;
 import com.cleanroommc.modularui.screen.viewport.ModularGuiContext;
 import com.cleanroommc.modularui.theme.WidgetSlotTheme;
 import com.cleanroommc.modularui.theme.WidgetTheme;
 import com.cleanroommc.modularui.utils.Alignment;
 import com.cleanroommc.modularui.utils.Color;
-import com.cleanroommc.modularui.utils.MouseData;
 import com.cleanroommc.modularui.utils.NumberFormat;
 import com.cleanroommc.modularui.value.sync.ItemSlotSH;
 import com.cleanroommc.modularui.value.sync.SyncHandler;
 import com.cleanroommc.modularui.widget.Widget;
-import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -43,9 +37,13 @@ import net.minecraftforge.items.IItemHandlerModifiable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interactable, JeiGhostIngredientSlot<ItemStack>, JeiIngredientProvider {
+public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interactable, JeiIngredientProvider {
 
     public static final int SIZE = 18;
+
+    public static ItemSlot create(boolean phantom) {
+        return phantom ? new PhantomItemSlot() : new ItemSlot();
+    }
 
     private static final TextRenderer textRenderer = new TextRenderer();
     private ItemSlotSH syncHandler;
@@ -65,7 +63,6 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
             throw new IllegalStateException("Overlays can't have slots!");
         }
         size(SIZE);
-        getContext().getJeiSettings().addJeiGhostIngredientSlot(this);
     }
 
     @Override
@@ -90,12 +87,11 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
         drawSlot(getSlot());
         RenderHelper.enableStandardItemLighting();
         GlStateManager.disableLighting();
-        if (this.syncHandler.isPhantom() && ModularUI.isJeiLoaded() &&
-                (ModularUIJeiPlugin.hasDraggingGhostIngredient() || ModularUIJeiPlugin.hoveringOverIngredient(this))) {
-            GlStateManager.colorMask(true, true, true, false);
-            drawHighlight(getArea(), isHovering());
-            GlStateManager.colorMask(true, true, true, true);
-        } else if (isHovering()) {
+        drawOverlay();
+    }
+
+    protected void drawOverlay() {
+        if (isHovering()) {
             GlStateManager.colorMask(true, true, true, false);
             GuiDraw.drawRect(1, 1, 16, 16, getSlotHoverColor());
             GlStateManager.colorMask(true, true, true, true);
@@ -130,38 +126,18 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
 
     @Override
     public @NotNull Result onMousePressed(int mouseButton) {
-        if (this.syncHandler.isPhantom()) {
-            MouseData mouseData = MouseData.create(mouseButton);
-            this.syncHandler.syncToServer(2, mouseData::writeToPacket);
-        } else {
-            ClientScreenHandler.clickSlot(getScreen(), getSlot());
-            //getScreen().getScreenWrapper().clickSlot();
-        }
+        ClientScreenHandler.clickSlot(getScreen(), getSlot());
         return Result.SUCCESS;
     }
 
     @Override
     public boolean onMouseRelease(int mouseButton) {
-        if (!this.syncHandler.isPhantom()) {
-            ClientScreenHandler.releaseSlot();
-            //getScreen().getScreenWrapper().releaseSlot();
-        }
+        ClientScreenHandler.releaseSlot();
         return true;
     }
 
     @Override
-    public boolean onMouseScroll(ModularScreen.UpOrDown scrollDirection, int amount) {
-        if (this.syncHandler.isPhantom()) {
-            MouseData mouseData = MouseData.create(scrollDirection.modifier);
-            this.syncHandler.syncToServer(3, mouseData::writeToPacket);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
     public void onMouseDrag(int mouseButton, long timeSinceClick) {
-        //getScreen().getScreenWrapper().dragSlot(timeSinceClick);
         ClientScreenHandler.dragSlot(timeSinceClick);
     }
 
@@ -172,6 +148,11 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
     @Override
     public Slot getVanillaSlot() {
         return this.syncHandler.getSlot();
+    }
+
+    @Override
+    public boolean handleAsVanillaSlot() {
+        return true;
     }
 
     @Override
@@ -200,34 +181,36 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
         GuiContainerAccessor acc = (GuiContainerAccessor) guiScreen;
         RenderItem renderItem = ((GuiScreenAccessor) guiScreen).getItemRender();
         ItemStack itemstack = slotIn.getStack();
-        boolean flag = false;
+        boolean isDragPreview = false;
         boolean flag1 = slotIn == acc.getClickedSlot() && !acc.getDraggedStack().isEmpty() && !acc.getIsRightMouseClick();
         ItemStack itemstack1 = guiScreen.mc.player.inventory.getItemStack();
         int amount = -1;
         String format = null;
 
-        if (slotIn == acc.getClickedSlot() && !acc.getDraggedStack().isEmpty() && acc.getIsRightMouseClick() && !itemstack.isEmpty()) {
-            itemstack = itemstack.copy();
-            itemstack.setCount(itemstack.getCount() / 2);
-        } else if (acc.getDragSplitting() && acc.getDragSplittingSlots().contains(slotIn) && !itemstack1.isEmpty()) {
-            if (acc.getDragSplittingSlots().size() == 1) {
-                return;
-            }
-
-            if (Container.canAddItemToSlot(slotIn, itemstack1, true) && getScreen().getContainer().canDragIntoSlot(slotIn)) {
-                itemstack = itemstack1.copy();
-                flag = true;
-                Container.computeStackSize(acc.getDragSplittingSlots(), acc.getDragSplittingLimit(), itemstack, slotIn.getStack().isEmpty() ? 0 : slotIn.getStack().getCount());
-                int k = Math.min(itemstack.getMaxStackSize(), slotIn.getItemStackLimit(itemstack));
-
-                if (itemstack.getCount() > k) {
-                    amount = k;
-                    format = TextFormatting.YELLOW.toString();
-                    itemstack.setCount(k);
+        if (!getSyncHandler().isPhantom()) {
+            if (slotIn == acc.getClickedSlot() && !acc.getDraggedStack().isEmpty() && acc.getIsRightMouseClick() && !itemstack.isEmpty()) {
+                itemstack = itemstack.copy();
+                itemstack.setCount(itemstack.getCount() / 2);
+            } else if (acc.getDragSplitting() && acc.getDragSplittingSlots().contains(slotIn) && !itemstack1.isEmpty()) {
+                if (acc.getDragSplittingSlots().size() == 1) {
+                    return;
                 }
-            } else {
-                acc.getDragSplittingSlots().remove(slotIn);
-                acc.invokeUpdateDragSplitting();
+
+                if (Container.canAddItemToSlot(slotIn, itemstack1, true) && getScreen().getContainer().canDragIntoSlot(slotIn)) {
+                    itemstack = itemstack1.copy();
+                    isDragPreview = true;
+                    Container.computeStackSize(acc.getDragSplittingSlots(), acc.getDragSplittingLimit(), itemstack, slotIn.getStack().isEmpty() ? 0 : slotIn.getStack().getCount());
+                    int k = Math.min(itemstack.getMaxStackSize(), slotIn.getItemStackLimit(itemstack));
+
+                    if (itemstack.getCount() > k) {
+                        amount = k;
+                        format = TextFormatting.YELLOW.toString();
+                        itemstack.setCount(k);
+                    }
+                } else {
+                    acc.getDragSplittingSlots().remove(slotIn);
+                    acc.invokeUpdateDragSplitting();
+                }
             }
         }
 
@@ -235,7 +218,7 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
         renderItem.zLevel = 100.0F;
 
         if (!flag1) {
-            if (flag) {
+            if (isDragPreview) {
                 GuiDraw.drawRect(1, 1, 16, 16, -2130706433);
             }
 
@@ -285,21 +268,6 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
 
         ((GuiAccessor) guiScreen).setZLevel(0f);
         renderItem.zLevel = 0f;
-    }
-
-    @Override
-    public void setGhostIngredient(@NotNull ItemStack ingredient) {
-        if (this.syncHandler.isPhantom()) {
-            this.syncHandler.updateFromClient(ingredient);
-        }
-    }
-
-    @Override
-    public @Nullable ItemStack castGhostIngredientIfValid(@NotNull Object ingredient) {
-        return areAncestorsEnabled() &&
-                this.syncHandler.isPhantom() &&
-                ingredient instanceof ItemStack itemStack &&
-                this.syncHandler.isItemValid(itemStack) ? itemStack : null;
     }
 
     @Override

GTNewHorizons/ModularUI2

(cherry picked from commit d4ba9a9c89cf04667341a13acfaa97f09f756356)

src/main/java/com/cleanroommc/modularui/factory/ClientGUI.java
@@ -1,15 +1,20 @@
 package com.cleanroommc.modularui.factory;
 
 import com.cleanroommc.modularui.api.MCHelper;
-import com.cleanroommc.modularui.screen.ContainerCustomizer;
-import com.cleanroommc.modularui.screen.ModularScreen;
 import com.cleanroommc.modularui.screen.NEISettingsImpl;
+import com.cleanroommc.modularui.screen.ModularContainer;
+import com.cleanroommc.modularui.screen.ModularScreen;
+import com.cleanroommc.modularui.screen.UISettings;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 import net.minecraft.client.gui.GuiScreen;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
 
 /**
  * Helper class to open client only GUIs. This class is safe to use inside a Modular GUI.
@@ -28,7 +33,7 @@ public class ClientGUI {
      * @param screen new modular screen
      */
     public static void open(@NotNull ModularScreen screen) {
-        open(screen, new NEISettingsImpl(), null);
+        open(screen, new UISettings());
     }
 
     /**
@@ -39,18 +44,20 @@ public class ClientGUI {
      * @param neiSettings custom NEI settings
      */
     public static void open(@NotNull ModularScreen screen, @NotNull NEISettingsImpl neiSettings) {
-        GuiManager.openScreen(screen, neiSettings, null);
+        GuiManager.openScreen(screen, new UISettings(neiSettings));
     }
 
     /**
      * Opens a modular screen on the next client tick with custom NEI settings.
      * It needs to be opened in next tick, because we might break the current GUI if we open it now.
      *
-     * @param screen              new modular screen
-     * @param containerCustomizer container customizer
+     * @param screen    new modular screen
+     * @param container custom container
      */
-    public static void open(@NotNull ModularScreen screen, @NotNull ContainerCustomizer containerCustomizer) {
-        GuiManager.openScreen(screen, new NEISettingsImpl(), containerCustomizer);
+    public static void open(@NotNull ModularScreen screen, @Nullable Supplier<ModularContainer> container) {
+        UISettings settings = new UISettings();
+        settings.customContainer(container);
+        GuiManager.openScreen(screen, settings);
     }
 
     /**
@@ -59,10 +66,23 @@ public class ClientGUI {
      *
      * @param screen              new modular screen
      * @param neiSettings         custom NEI settings
-     * @param containerCustomizer container customizer
+     * @param container   custom container
      */
-    public static void open(@NotNull ModularScreen screen, @NotNull NEISettingsImpl neiSettings, @Nullable ContainerCustomizer containerCustomizer) {
-        GuiManager.openScreen(screen, neiSettings, containerCustomizer);
+    public static void open(@NotNull ModularScreen screen, @NotNull NEISettingsImpl neiSettings, @Nullable Supplier<ModularContainer> container) {
+        UISettings settings = new UISettings(neiSettings);
+        settings.customContainer(container);
+        GuiManager.openScreen(screen, settings);
+    }
+
+    /**
+     * Opens a modular screen on the next client tick with custom jei settings.
+     * It needs to be opened in next tick, because we might break the current GUI if we open it now.
+     *
+     * @param screen   new modular screen
+     * @param settings ui settings
+     */
+    public static void open(@NotNull ModularScreen screen, @NotNull UISettings settings) {
+        GuiManager.openScreen(screen, settings);
     }
 
     /**

src/main/java/com/cleanroommc/modularui/widgets/slot/ItemSlot.java
@@ -1,4 +1,4 @@
-package com.cleanroommc.modularui.widgets;
+package com.cleanroommc.modularui.widgets.slot;
 
 import com.cleanroommc.modularui.api.ITheme;
 import com.cleanroommc.modularui.api.widget.IVanillaSlot;
@@ -6,7 +6,6 @@ import com.cleanroommc.modularui.api.widget.Interactable;
 import com.cleanroommc.modularui.drawable.GuiDraw;
 import com.cleanroommc.modularui.drawable.text.TextRenderer;
 import com.cleanroommc.modularui.utils.item.IItemHandlerModifiable;
-import com.cleanroommc.modularui.integration.nei.NEIDragAndDropHandler;
 import com.cleanroommc.modularui.integration.nei.NEIIngredientProvider;
 import com.cleanroommc.modularui.screen.ClientScreenHandler;
 import com.cleanroommc.modularui.mixins.early.minecraft.GuiAccessor;
@@ -25,7 +24,6 @@ import com.cleanroommc.modularui.utils.NumberFormat;
 import com.cleanroommc.modularui.value.sync.ItemSlotSH;
 import com.cleanroommc.modularui.value.sync.SyncHandler;
 import com.cleanroommc.modularui.widget.Widget;
-import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -42,9 +40,13 @@ import net.minecraft.util.EnumChatFormatting;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interactable, NEIDragAndDropHandler, NEIIngredientProvider {
+public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interactable, NEIIngredientProvider {
 
     public static final int SIZE = 18;
+
+    public static ItemSlot create(boolean phantom) {
+        return phantom ? new PhantomItemSlot() : new ItemSlot();
+    }
 
     private static final TextRenderer textRenderer = new TextRenderer();
     private ItemSlotSH syncHandler;
@@ -87,6 +89,10 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
         RenderHelper.enableGUIStandardItemLighting();
         drawSlot(getSlot());
         RenderHelper.disableStandardItemLighting();
+        drawOverlay();
+    }
+
+    protected void drawOverlay() {
         if (isHovering()) {
             GlStateManager.disableLighting();
             GlStateManager.enableBlend();
@@ -125,38 +131,18 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
 
     @Override
     public @NotNull Result onMousePressed(int mouseButton) {
-        if (this.syncHandler.isPhantom()) {
-            MouseData mouseData = MouseData.create(mouseButton);
-            this.syncHandler.syncToServer(2, mouseData::writeToPacket);
-        } else {
-            ClientScreenHandler.clickSlot(getScreen(), getSlot());
-            //getScreen().getScreenWrapper().clickSlot();
-        }
+        ClientScreenHandler.clickSlot(getScreen(), getSlot());
         return Result.SUCCESS;
     }
 
     @Override
     public boolean onMouseRelease(int mouseButton) {
-        if (!this.syncHandler.isPhantom()) {
-            ClientScreenHandler.releaseSlot();
-            //getScreen().getScreenWrapper().releaseSlot();
-        }
+        ClientScreenHandler.releaseSlot();
         return true;
     }
 
     @Override
-    public boolean onMouseScroll(ModularScreen.UpOrDown scrollDirection, int amount) {
-        if (this.syncHandler.isPhantom()) {
-            MouseData mouseData = MouseData.create(scrollDirection.modifier);
-            this.syncHandler.syncToServer(3, mouseData::writeToPacket);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
     public void onMouseDrag(int mouseButton, long timeSinceClick) {
-        //getScreen().getScreenWrapper().dragSlot(timeSinceClick);
         ClientScreenHandler.dragSlot(timeSinceClick);
     }
 
@@ -167,6 +153,11 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
     @Override
     public Slot getVanillaSlot() {
         return this.syncHandler.getSlot();
+    }
+
+    @Override
+    public boolean handleAsVanillaSlot() {
+        return true;
     }
 
     @Override
@@ -195,36 +186,38 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
         GuiContainerAccessor acc = (GuiContainerAccessor) guiScreen;
         RenderItem renderItem = GuiScreenAccessor.getItemRender();
         ItemStack itemstack = slotIn.getStack();
-        boolean flag = false;
+        boolean isDragPreview = false;
         boolean flag1 = slotIn == acc.getClickedSlot() && acc.getDraggedStack() != null && !acc.getIsRightMouseClick();
         ItemStack itemstack1 = guiScreen.mc.thePlayer.inventory.getItemStack();
         int amount = -1;
         String format = null;
 
-        if (slotIn == acc.getClickedSlot() && acc.getDraggedStack() != null && acc.getIsRightMouseClick() && itemstack != null) {
-            itemstack = itemstack.copy();
-            itemstack.stackSize /= 2;
-        } else if (acc.getDragSplitting() && acc.getDragSplittingSlots().contains(slotIn) && itemstack1 != null) {
-            if (acc.getDragSplittingSlots().size() == 1) {
-                return;
-            }
-
-            // canAddItemToSlot
-            if (Container.func_94527_a(slotIn, itemstack1, true) && getScreen().getContainer().canDragIntoSlot(slotIn)) {
-                itemstack = itemstack1.copy();
-                flag = true;
-                // computeStackSize
-                Container.func_94525_a(acc.getDragSplittingSlots(), acc.getDragSplittingLimit(), itemstack, slotIn.getStack() == null ? 0 : slotIn.getStack().stackSize);
-                int k = Math.min(itemstack.getMaxStackSize(), slotIn.getSlotStackLimit());
-
-                if (itemstack.stackSize > k) {
-                    amount = k;
-                    format = EnumChatFormatting.YELLOW.toString();
-                    itemstack.stackSize = k;
+        if (!getSyncHandler().isPhantom()) {
+            if (slotIn == acc.getClickedSlot() && acc.getDraggedStack() != null && acc.getIsRightMouseClick() && itemstack != null) {
+                itemstack = itemstack.copy();
+                itemstack.stackSize /= 2;
+            } else if (acc.getDragSplitting() && acc.getDragSplittingSlots().contains(slotIn) && itemstack1 != null) {
+                if (acc.getDragSplittingSlots().size() == 1) {
+                    return;
                 }
-            } else {
-                acc.getDragSplittingSlots().remove(slotIn);
-                acc.invokeUpdateDragSplitting();
+
+                // canAddItemToSlot
+                if (Container.func_94527_a(slotIn, itemstack1, true) && getScreen().getContainer().canDragIntoSlot(slotIn)) {
+                    itemstack = itemstack1.copy();
+                    isDragPreview = true;
+                    // computeStackSize
+                    Container.func_94525_a(acc.getDragSplittingSlots(), acc.getDragSplittingLimit(), itemstack, slotIn.getStack() == null ? 0 : slotIn.getStack().stackSize);
+                    int k = Math.min(itemstack.getMaxStackSize(), slotIn.getSlotStackLimit());
+
+                    if (itemstack.stackSize > k) {
+                        amount = k;
+                        format = EnumChatFormatting.YELLOW.toString();
+                        itemstack.stackSize = k;
+                    }
+                } else {
+                    acc.getDragSplittingSlots().remove(slotIn);
+                    acc.invokeUpdateDragSplitting();
+                }
             }
         }
 
@@ -232,7 +225,7 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
         renderItem.zLevel = 100.0F;
 
         if (!flag1) {
-            if (flag) {
+            if (isDragPreview) {
                 GuiDraw.drawRect(1, 1, 16, 16, -2130706433);
             }
 
@@ -286,14 +279,6 @@ public class ItemSlot extends Widget<ItemSlot> implements IVanillaSlot, Interact
 
         ((GuiAccessor) guiScreen).setZLevel(0f);
         renderItem.zLevel = 0f;
-    }
-
-    @Override
-    public boolean handleDragAndDrop(@NotNull ItemStack draggedStack, int button) {
-        if (!this.syncHandler.isPhantom()) return false;
-        this.syncHandler.updateFromClient(draggedStack, button);
-        draggedStack.stackSize = 0;
-        return true;
     }
 
     @Override