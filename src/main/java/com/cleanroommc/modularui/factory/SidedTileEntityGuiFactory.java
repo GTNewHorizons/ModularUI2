CleanroomMC/ModularUI

ability to open synced ui from client & baubles test

src/main/java/com/cleanroommc/modularui/factory/SidedTileEntityGuiFactory.java
@@ -1,6 +1,7 @@
 package com.cleanroommc.modularui.factory;
 
 import com.cleanroommc.modularui.api.IGuiHolder;
+import com.cleanroommc.modularui.utils.Platform;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -8,6 +9,8 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -19,15 +22,8 @@ public class SidedTileEntityGuiFactory extends AbstractUIFactory<SidedPosGuiData
 
     public <T extends TileEntity & IGuiHolder<SidedPosGuiData>> void open(EntityPlayer player, T tile, EnumFacing facing) {
         Objects.requireNonNull(player);
-        Objects.requireNonNull(tile);
         Objects.requireNonNull(facing);
-        if (tile.isInvalid()) {
-            throw new IllegalArgumentException("Can't open invalid TileEntity GUI!");
-        }
-        if (player.world != tile.getWorld()) {
-            throw new IllegalArgumentException("TileEntity must be in same dimension as the player!");
-        }
-        BlockPos pos = tile.getPos();
+        BlockPos pos = TileEntityGuiFactory.getPosFromTile(tile);
         SidedPosGuiData data = new SidedPosGuiData(player, pos.getX(), pos.getY(), pos.getZ(), facing);
         GuiManager.open(this, data, (EntityPlayerMP) player);
     }
@@ -38,6 +34,22 @@ public class SidedTileEntityGuiFactory extends AbstractUIFactory<SidedPosGuiData
         Objects.requireNonNull(facing);
         SidedPosGuiData data = new SidedPosGuiData(player, pos.getX(), pos.getY(), pos.getZ(), facing);
         GuiManager.open(this, data, (EntityPlayerMP) player);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public <T extends TileEntity & IGuiHolder<SidedPosGuiData>> void openClient(T tile, EnumFacing facing) {
+        Objects.requireNonNull(facing);
+        BlockPos pos = TileEntityGuiFactory.getPosFromTile(tile);
+        SidedPosGuiData data = new SidedPosGuiData(Platform.getClientPlayer(), pos.getX(), pos.getY(), pos.getZ(), facing);
+        GuiManager.openFromClient(this, data);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void openClient(BlockPos pos, EnumFacing facing) {
+        Objects.requireNonNull(pos);
+        Objects.requireNonNull(facing);
+        SidedPosGuiData data = new SidedPosGuiData(Platform.getClientPlayer(), pos.getX(), pos.getY(), pos.getZ(), facing);
+        GuiManager.openFromClient(this, data);
     }
 
     private SidedTileEntityGuiFactory() {

src/main/java/com/cleanroommc/modularui/network/packets/OpenGuiPacket.java
@@ -4,15 +4,15 @@ import com.cleanroommc.modularui.api.UIFactory;
 import com.cleanroommc.modularui.factory.GuiData;
 import com.cleanroommc.modularui.factory.GuiManager;
 import com.cleanroommc.modularui.network.IPacket;
-import com.cleanroommc.modularui.network.NetworkHandler;
 import com.cleanroommc.modularui.network.NetworkUtils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.PacketBuffer;
-
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
@@ -49,7 +49,14 @@ public class OpenGuiPacket<T extends GuiData> implements IPacket {
     @SideOnly(Side.CLIENT)
     @Override
     public @Nullable IPacket executeClient(NetHandlerPlayClient handler) {
-        GuiManager.open(this.windowId, this.factory, this.data, Minecraft.getMinecraft().player);
+        GuiManager.openFromClient(this.windowId, this.factory, this.data, Minecraft.getMinecraft().player);
+        return null;
+    }
+
+    @Override
+    public @Nullable IPacket executeServer(NetHandlerPlayServer handler) {
+        T guiData = this.factory.readGuiData(handler.player, this.data);
+        GuiManager.open(this.factory, guiData, handler.player);
         return null;
     }
 }

GTNewHorizons/ModularUI2

(cherry picked from commit cba480677347efb547a1533d1472d451a77c2d41)

src/main/java/com/cleanroommc/modularui/factory/SidedTileEntityGuiFactory.java
@@ -1,6 +1,8 @@
 package com.cleanroommc.modularui.factory;
 
 import com.cleanroommc.modularui.api.IGuiHolder;
+import com.cleanroommc.modularui.utils.Platform;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.PacketBuffer;
@@ -17,15 +19,9 @@ public class SidedTileEntityGuiFactory extends AbstractUIFactory<SidedPosGuiData
 
     public <T extends TileEntity & IGuiHolder<SidedPosGuiData>> void open(EntityPlayer player, T tile, ForgeDirection side) {
         Objects.requireNonNull(player);
-        Objects.requireNonNull(tile);
-        Objects.requireNonNull(side);
-        if (tile.isInvalid()) {
-            throw new IllegalArgumentException("Can't open invalid TileEntity GUI!");
-        }
-        if (player.worldObj != tile.getWorldObj()) {
-            throw new IllegalArgumentException("TileEntity must be in same dimension as the player!");
-        }
-        SidedPosGuiData data = new SidedPosGuiData(player, tile.xCoord, tile.yCoord, tile.zCoord, side);
+        Objects.requireNonNull(facing);
+        BlockPos pos = TileEntityGuiFactory.getPosFromTile(tile);
+        SidedPosGuiData data = new SidedPosGuiData(player, pos.getX(), pos.getY(), pos.getZ(), facing);
         GuiManager.open(this, data, (EntityPlayerMP) player);
     }
 
@@ -34,6 +30,22 @@ public class SidedTileEntityGuiFactory extends AbstractUIFactory<SidedPosGuiData
         Objects.requireNonNull(side);
         SidedPosGuiData data = new SidedPosGuiData(player, x, y, z, side);
         GuiManager.open(this, data, (EntityPlayerMP) player);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public <T extends TileEntity & IGuiHolder<SidedPosGuiData>> void openClient(T tile, ForgeDirection facing) {
+        Objects.requireNonNull(facing);
+        BlockPos pos = TileEntityGuiFactory.getPosFromTile(tile);
+        SidedPosGuiData data = new SidedPosGuiData(Platform.getClientPlayer(), pos.getX(), pos.getY(), pos.getZ(), facing);
+        GuiManager.openFromClient(this, data);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void openClient(BlockPos pos, ForgeDirection facing) {
+        Objects.requireNonNull(pos);
+        Objects.requireNonNull(facing);
+        SidedPosGuiData data = new SidedPosGuiData(Platform.getClientPlayer(), pos.getX(), pos.getY(), pos.getZ(), facing);
+        GuiManager.openFromClient(this, data);
     }
 
     private SidedTileEntityGuiFactory() {

src/main/java/com/cleanroommc/modularui/network/packets/OpenGuiPacket.java
@@ -4,7 +4,6 @@ import com.cleanroommc.modularui.factory.GuiData;
 import com.cleanroommc.modularui.factory.GuiManager;
 import com.cleanroommc.modularui.api.UIFactory;
 import com.cleanroommc.modularui.network.IPacket;
-import com.cleanroommc.modularui.network.NetworkHandler;
 import com.cleanroommc.modularui.network.NetworkUtils;
 
 import cpw.mods.fml.relauncher.Side;
@@ -12,6 +11,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.PacketBuffer;
 import org.jetbrains.annotations.Nullable;
 
@@ -49,7 +49,14 @@ public class OpenGuiPacket<T extends GuiData> implements IPacket {
     @SideOnly(Side.CLIENT)
     @Override
     public @Nullable IPacket executeClient(NetHandlerPlayClient handler) {
-        GuiManager.open(this.windowId, this.factory, this.data, Minecraft.getMinecraft().thePlayer);
+        GuiManager.openFromClient(this.windowId, this.factory, this.data, Minecraft.getMinecraft().thePlayer);
+        return null;
+    }
+
+    @Override
+    public @Nullable IPacket executeServer(NetHandlerPlayServer handler) {
+        T guiData = this.factory.readGuiData(handler.player, this.data);
+        GuiManager.open(this.factory, guiData, handler.player);
         return null;
     }
 }