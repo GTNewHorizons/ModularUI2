8664981410df91e3211b0bd78ba2ff4373e3093f
Yannick Marcotte-Gourde <yannickmg@gmail.com>
2025-05-22 18:32:07 -0400

    Add grouping support to number text fields (#34)

--- src/main/java/com/cleanroommc/modularui/widgets/textfield/TextFieldRenderer.java
+++ src/main/java/com/cleanroommc/modularui/widgets/textfield/TextFieldRenderer.java
@@ -8,17 +8,31 @@ import com.cleanroommc.modularui.utils.Platform;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
+
 import java.awt.*;
+import java.text.DecimalFormat;
 import java.awt.geom.Point2D;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TextFieldRenderer extends TextRenderer {
+    // 1.7.10 only until proper number groupings support
+    private static final DecimalFormat INTEGER_FIELD_FORMAT = new DecimalFormat("#");
+    private static final char groupingSeparator = INTEGER_FIELD_FORMAT.getDecimalFormatSymbols().getGroupingSeparator();
+
+    static {
+        INTEGER_FIELD_FORMAT.setGroupingUsed(true);
+        INTEGER_FIELD_FORMAT.setGroupingSize(3);
+    }
 
     protected final TextFieldHandler handler;
     protected int markedColor = 0x2F72A8;
     protected int cursorColor = 0xFFFFFFFF;
     protected boolean renderCursor = false;
+    private boolean formatAsInteger = false;
 
     public TextFieldRenderer(TextFieldHandler handler) {
         this.handler = handler;
@@ -38,6 +52,34 @@ public class TextFieldRenderer extends TextRenderer {
 
     public void setCursorColor(int cursorColor) {
         this.cursorColor = cursorColor;
+    }
+
+    // 1.7.10 only until proper number groupings support
+    @ApiStatus.Experimental
+    public void setFormatAsInteger(boolean formatAsInteger) {
+        this.formatAsInteger = formatAsInteger;
+    }
+
+    // 1.7.10 only until proper number groupings support
+    @Override
+    public void draw(List<String> lines) {
+        if (formatAsInteger) lines = decorateLines(lines);
+        super.draw(lines);
+    }
+
+    // 1.7.10 only until proper number groupings support
+    private static @NotNull List<String> decorateLines(List<String> lines) {
+        return lines.stream().map(TextFieldRenderer::tryFormatString)
+                .collect(Collectors.toList());
+    }
+
+    // 1.7.10 only until proper number groupings support
+    private static @NotNull String tryFormatString(String str) {
+        try {
+            return INTEGER_FIELD_FORMAT.format(Long.parseLong(str));
+        } catch (NumberFormatException e) {
+            return str;
+        }
     }
 
     @Override
@@ -94,28 +136,51 @@ public class TextFieldRenderer extends TextRenderer {
         if (lines.isEmpty()) {
             return new Point();
         }
+        if (formatAsInteger) lines = decorateLines(lines);
         List<Line> measuredLines = measureLines(lines);
         y -= getStartY(measuredLines.size());
         int index = (int) (y / (getFontHeight()));
         if (index < 0) return new Point();
-        if (index >= measuredLines.size())
-            return new Point(measuredLines.get(measuredLines.size() - 1).getText().length(), measuredLines.size() - 1);
+        if (index >= measuredLines.size()) {
+            return new Point(getRealLength(measuredLines.get(measuredLines.size() - 1).getText()), measuredLines.size() - 1);
+        }
         Line line = measuredLines.get(index);
         x -= getStartX(line.getWidth());
         if (line.getWidth() <= 0) return new Point(0, index);
-        if (line.getWidth() < x) return new Point(line.getText().length(), index);
+        if (line.getWidth() < x) {
+            return new Point(getRealLength(line.getText()), index);
+        }
         float currentX = 0;
+        int ignoredChars = 0;
         for (int i = 0; i < line.getText().length(); i++) {
             char c = line.getText().charAt(i);
             float charWidth = getFontRenderer().getCharWidth(c) * this.scale;
             currentX += charWidth;
-            if (currentX >= x) {
+            if (isIgnoredChar(c)) {
+                ignoredChars++;
+            } else if (currentX >= x) {
                 // dist with current letter < dist without current letter -> next letter pos
                 if (Math.abs(currentX - x) < Math.abs(currentX - charWidth - x)) i++;
-                return new Point(i, index);
+                return new Point(i - ignoredChars, index);
             }
         }
         return new Point();
+    }
+
+    // 1.7.10 only until proper number groupings support
+    /**
+     * Whether the given character should be ignored for cursor positioning purposes
+     */
+    @ApiStatus.Experimental
+    protected boolean isIgnoredChar(int c) {
+        return formatAsInteger && c == groupingSeparator;
+    }
+
+    // 1.7.10 only until proper number groupings support
+    private int getRealLength(String text) {
+        int length = text.length();
+        if (formatAsInteger) length -= (int) text.chars().filter(this::isIgnoredChar).count();
+        return length;
     }
 
     public Point2D.Float getPosOf(List<Line> measuredLines, Point cursorPos) {
@@ -123,8 +188,30 @@ public class TextFieldRenderer extends TextRenderer {
             return new Point2D.Float(getStartX(0), getStartYOfLines(1));
         }
         Line line = measuredLines.get(cursorPos.y);
-        String sub = line.getText().substring(0, Math.min(line.getText().length(), cursorPos.x));
+        String sub = getStringBeforeCursor(line, cursorPos);
         return new Point2D.Float(getStartX(line.getWidth()) + getFontRenderer().getStringWidth(sub) * this.scale, getStartYOfLines(measuredLines.size()) + cursorPos.y * getFontHeight());
+    }
+
+    // 1.7.10 only until proper number groupings support
+    @ApiStatus.Experimental()
+    private @NotNull String getStringBeforeCursor(Line line, Point cursorPos) {
+        String text = line.getText();
+        String sub = text.substring(0, Math.min(text.length(), cursorPos.x));
+        if (formatAsInteger) {
+            int i = 0;
+            int ignoredChars = 0;
+            while (i < sub.length() && i + ignoredChars < text.length()) {
+                if (isIgnoredChar(text.charAt(i + ignoredChars))) {
+                    ignoredChars++;
+                } else {
+                    i++;
+                }
+            }
+            if (ignoredChars > 0)         {
+                sub = sub + text.substring(sub.length(), Math.min(text.length(), cursorPos.x + ignoredChars));
+            }
+        }
+        return sub;
     }
 
     @SideOnly(Side.CLIENT)
