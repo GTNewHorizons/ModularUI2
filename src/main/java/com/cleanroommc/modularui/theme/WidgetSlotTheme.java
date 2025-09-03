d9dd075e16ccf1c9f3fd43fd04055b757738325e
Miya <39800618+rspforhp@users.noreply.github.com>
2025-09-03 21:44:30 +0500

    Add support for themes (#40)

--- src/main/java/com/cleanroommc/modularui/theme/WidgetSlotTheme.java
+++ src/main/java/com/cleanroommc/modularui/theme/WidgetSlotTheme.java
@@ -9,18 +9,31 @@ import com.google.gson.JsonObject;
 public class WidgetSlotTheme extends WidgetTheme {
 
     private final int slotHoverColor;
+    private final boolean useCustomSlotTextures;
+    private final IDrawable inventorySlotBackground;
+    private final IDrawable hotbarSlotBackground;
 
-    public WidgetSlotTheme(IDrawable background, int slotHoverColor) {
+
+    public WidgetSlotTheme(IDrawable background, int slotHoverColor,boolean useCustomSlotTextures,IDrawable inventory,IDrawable hotbar) {
         super(background, null, Color.WHITE.main, 0xFF404040, false);
         this.slotHoverColor = slotHoverColor;
+        this.useCustomSlotTextures=useCustomSlotTextures;
+        this.inventorySlotBackground=inventory;
+        this.hotbarSlotBackground=hotbar;
     }
 
     public WidgetSlotTheme(WidgetTheme parent, JsonObject json, JsonObject fallback) {
         super(parent, json, fallback);
         this.slotHoverColor = JsonHelper.getColorWithFallback(json, fallback, ((WidgetSlotTheme) parent).getSlotHoverColor(), IThemeApi.SLOT_HOVER_COLOR);
+        this.useCustomSlotTextures=JsonHelper.getBoolWithFallback(json, fallback, ((WidgetSlotTheme) parent).getUseCustomSlotTextures(), IThemeApi.SLOT_CUSTOM_TEXTURES);
+        this.inventorySlotBackground = JsonHelper.deserializeWithFallback(json, fallback, IDrawable.class, ((WidgetSlotTheme) parent).getInventorySlotBackground(), IThemeApi.SLOT_INVENTORY_BACKGROUND);
+        this.hotbarSlotBackground = JsonHelper.deserializeWithFallback(json, fallback, IDrawable.class, ((WidgetSlotTheme) parent).getHotbarSlotBackground(), IThemeApi.SLOT_HOTBAR_BACKGROUND);
     }
 
     public int getSlotHoverColor() {
         return this.slotHoverColor;
     }
+    public boolean getUseCustomSlotTextures() {return this.useCustomSlotTextures;}
+    public IDrawable getInventorySlotBackground() {return this.inventorySlotBackground;}
+    public IDrawable getHotbarSlotBackground() {return this.hotbarSlotBackground;}
 }
