985b1808730a1278d1ce2d57295e347f44506a9d
chrombread <chrombread8@gmail.com>
2025-11-08 12:43:25 -0800

    change method name

--- src/main/java/com/cleanroommc/modularui/widgets/layout/Flow.java
+++ src/main/java/com/cleanroommc/modularui/widgets/layout/Flow.java
@@ -259,7 +259,7 @@ public class Flow extends ParentWidget<Flow> implements ILayoutWidget, IExpander
      * @param doReverse true if the children list should be read in reverse
      * @return this
      */
-    public Flow shouldReverseChildren(boolean doReverse) {
+    public Flow reverseLayout(boolean doReverse) {
         this.reverseChildren = doReverse;
         return this;
     }
