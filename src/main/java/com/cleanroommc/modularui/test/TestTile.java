b34af44b57951c9da11721868cecb3d9841d275f
chrombread <chrombread8@gmail.com>
2025-11-08 23:26:49 -0800

    formatting

--- src/main/java/com/cleanroommc/modularui/test/TestTile.java
+++ src/main/java/com/cleanroommc/modularui/test/TestTile.java
@@ -275,7 +275,8 @@ public class TestTile extends TileEntity implements IGuiHolder<PosGuiData> {
                                                                                 })
                                                                                 //.flex(flex -> flex.left(3)) // ?
                                                                                 .overlay(IKey.str("Button 2")))
-                                                                        .child(new TextFieldWidget().tooltip(t->t.addLine("hello, i am overridden!"))
+                                                                        .child(new TextFieldWidget()
+                                                                                .tooltip(t->t.addLine("hello, i am overridden!"))
                                                                                 .size(60, 18)
                                                                                 .setTextAlignment(Alignment.Center)
                                                                                 .value(SyncHandlers.string(() -> this.value, val -> this.value = val))
