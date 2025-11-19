e0280114dedfb3caeb3a44eac272f5dbc827cd9d
RealSilverMoon <qazhhhh@outlook.com>
2025-11-19 20:44:47 +0800

    Add verify before executing

--- src/main/java/com/cleanroommc/modularui/widgets/textfield/BaseTextFieldWidget.java
+++ src/main/java/com/cleanroommc/modularui/widgets/textfield/BaseTextFieldWidget.java
@@ -144,7 +144,7 @@ public class BaseTextFieldWidget<W extends BaseTextFieldWidget<W>> extends Abstr
 
     @Override
     public boolean isFocused() {
-        return getContext().isFocused(this);
+        return isValid() && getContext().isFocused(this);
     }
 
     @Override
