d58ede2ce63703e8591b03d97855f57eb41f9bc2
chrombread <chrombread8@gmail.com>
2025-11-22 01:43:37 -0800

    change to using guidraw methods instead.

--- src/main/java/com/cleanroommc/modularui/widgets/EntityDisplayWidget.java
+++ src/main/java/com/cleanroommc/modularui/widgets/EntityDisplayWidget.java
@@ -1,25 +1,38 @@
 package com.cleanroommc.modularui.widgets;
 
+import com.cleanroommc.modularui.api.drawable.IDrawable;
+import com.cleanroommc.modularui.drawable.GuiDraw;
+import com.cleanroommc.modularui.screen.viewport.GuiContext;
 import com.cleanroommc.modularui.screen.viewport.ModularGuiContext;
+import com.cleanroommc.modularui.theme.WidgetTheme;
 import com.cleanroommc.modularui.theme.WidgetThemeEntry;
 import com.cleanroommc.modularui.utils.Platform;
 import com.cleanroommc.modularui.widget.Widget;
 import com.cleanroommc.modularui.widget.sizer.Area;
 
 import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import org.lwjgl.opengl.GL11;
 
+import javax.annotation.Nullable;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class EntityDisplayWidget extends Widget<EntityDisplayWidget>{
+/**
+ *  See {@link com.cleanroommc.modularui.drawable.GuiDraw#drawEntity(Entity, float, float, float, float, float, Consumer, Consumer)}
+ *  The consumers are only called if the lookAtMouse is not enabled.
+ */
+public class EntityDisplayWidget implements IDrawable {
+
     private final Supplier<EntityLivingBase> entitySupplier;
     private  boolean lookAtMouse = false;
-    private  float mouseFollowStrength = 1f;
+
+    @Nullable Consumer<EntityLivingBase> preDraw = null;
+    @Nullable Consumer<EntityLivingBase> postDraw = null;
 
     public EntityDisplayWidget(Supplier<EntityLivingBase> e) {
         this.entitySupplier = e;
-        this.size(18, 18);
     }
 
     public EntityDisplayWidget doesLookAtMouse(boolean doesLookAtMouse) {
@@ -27,43 +40,22 @@ public class EntityDisplayWidget extends Widget<EntityDisplayWidget>{
         return this;
     }
 
-    public EntityDisplayWidget mouseFollowStrength(float strength) {
-        this.mouseFollowStrength = strength;
+    public EntityDisplayWidget preDraw(Consumer<EntityLivingBase> preDraw) {
+        this.preDraw = preDraw;
+        return this;
+    }
+    public EntityDisplayWidget postDraw(Consumer<EntityLivingBase> postDraw) {
+        this.postDraw = postDraw;
         return this;
     }
 
-    public static void drawEntity(int x, int y, int scale, EntityLivingBase e, boolean lookAtMouse, float mouseX,
-                                  float mouseY, float mouseFollowStrength) {
-        if (e == null) return;
-        GL11.glColor4f(1, 1, 1, 1);
-        Platform.setupDrawItem();
-        float yawX = 0;
-        float pitchY = 0;
-        if (lookAtMouse) {
-            yawX = -mouseX;
-            pitchY -= mouseY;
-            yawX /= scale;
-            pitchY /= scale;
-            yawX *= mouseFollowStrength;
-            pitchY *= mouseFollowStrength;
-        }
-        GuiInventory.func_147046_a(x, y, scale, yawX, pitchY, e);
-        Platform.endDrawItem();
-    }
-
     @Override
-    public void draw(ModularGuiContext context, WidgetThemeEntry<?> widgetTheme) {
-        EntityLivingBase e = entitySupplier.get();
-        Area area = this.getArea();
-        drawEntity(
-                +area.width / 2,
-                area.height,
-                area.width,
-                e,
-                this.lookAtMouse,
-                context.getMouseX(),
-                context.getMouseY(),
-                mouseFollowStrength);
+    public void draw(GuiContext context, int x, int y, int width, int height, WidgetTheme widgetTheme) {
+        if (entitySupplier == null || entitySupplier.get() == null) return;
+        if (this.lookAtMouse) {
+            GuiDraw.drawEntityLookingAtMouse(entitySupplier.get(), x, y, width, height, context.getCurrentDrawingZ(), context.getMouseX(), context.getMouseY() );
+        } else {
+            GuiDraw.drawEntity(entitySupplier.get(),x,y,width,height,context.getCurrentDrawingZ(), preDraw, postDraw);
+        }
     }
-
 }
