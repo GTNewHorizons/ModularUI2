f308144c3a45ed2f16125ba28b956d97c90c3505
Maya <10861407+serenibyss@users.noreply.github.com>
2025-11-24 11:31:07 -0700

    Port MUI1 fluid util classes to MUI2 (#74)

---
+++ src/main/java/com/cleanroommc/modularui/utils/fluid/IFluidTanksHandler.java
@@ -0,0 +1,56 @@
+package com.cleanroommc.modularui.utils.fluid;
+
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface IFluidTanksHandler {
+
+    int getTanks();
+
+    @Nullable
+    FluidStack getFluidStackInTank(int tank);
+
+    Fluid getFluidInTank(int tank);
+
+    IFluidTankLong getFluidTank(int tank);
+
+    @Nullable
+    FluidStack fill(int tank, @Nullable Fluid fluid, long amount, boolean simulate);
+
+    @Nullable
+    FluidStack drain(int tank, long amount, boolean simulate);
+
+    long getTankCapacity(int tank);
+
+    long getRealTankCapacity(int tank);
+
+    long getTankStoredAmount(int tank);
+
+    default boolean isFluidValid(int slot, FluidStack stack) {
+        return true;
+    }
+
+    default List<FluidStack> getFluids() {
+        List<FluidStack> ret = new ArrayList<>();
+
+        for (int i = 0; i < this.getTanks(); ++i) {
+            ret.add(this.getFluidStackInTank(i));
+        }
+
+        return ret;
+    }
+
+    void setFluidInTank(int tank, Fluid fluid, long amount);
+
+    default void setFluidInTank(int tank, Fluid fluid) {
+        setFluidInTank(tank, fluid, 0);
+    }
+
+    default void setFluidInTank(int tank, IFluidTankLong fluid) {
+        setFluidInTank(tank, fluid != null ? fluid.getStoredFluid() : null, fluid != null ? fluid.getFluidAmount() : 0);
+    }
+}
