CleanroomMC/ModularUI

add register synced action overload with side param

src/main/java/com/cleanroommc/modularui/value/sync/PanelSyncManager.java
@@ -13,6 +13,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.items.wrapper.PlayerMainInvWrapper;
 
 import io.netty.buffer.Unpooled;
@@ -329,6 +330,10 @@ public class PanelSyncManager {
 
     public PanelSyncManager registerSyncedAction(String mapKey, ISyncedAction action) {
         return registerSyncedAction(mapKey, true, true, action);
+    }
+
+    public PanelSyncManager registerSyncedAction(String mapKey, Side side, ISyncedAction action) {
+        return registerSyncedAction(mapKey, side.isClient(), side.isServer(), action);
     }
 
     public PanelSyncManager registerClientSyncedAction(String mapKey, ISyncedAction action) {

GTNewHorizons/ModularUI2

(cherry picked from commit 08a3aa5355b5d0689296e1f98e47d8723631bc42)

src/main/java/com/cleanroommc/modularui/value/sync/PanelSyncManager.java
@@ -14,6 +14,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
+import cpw.mods.fml.relauncher.Side;
 
 import io.netty.buffer.Unpooled;
 import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
@@ -337,6 +338,10 @@ public class PanelSyncManager {
 
     public PanelSyncManager registerSyncedAction(String mapKey, ISyncedAction action) {
         return registerSyncedAction(mapKey, true, true, action);
+    }
+
+    public PanelSyncManager registerSyncedAction(String mapKey, Side side, ISyncedAction action) {
+        return registerSyncedAction(mapKey, side.isClient(), side.isServer(), action);
     }
 
     public PanelSyncManager registerClientSyncedAction(String mapKey, ISyncedAction action) {