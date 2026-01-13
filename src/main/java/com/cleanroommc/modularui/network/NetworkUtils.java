CleanroomMC/ModularUI

rework syncing so inactive screens can still receive packets

src/main/java/com/cleanroommc/modularui/network/NetworkUtils.java
@@ -12,6 +12,7 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
@@ -107,6 +108,10 @@ public class NetworkUtils {
             buffer.writeVarInt(Short.MAX_VALUE + 1);
             return;
         }
+        if (string.isEmpty()) {
+            buffer.writeVarInt(0);
+            return;
+        }
         byte[] bytesTest = string.getBytes(StandardCharsets.UTF_8);
         byte[] bytes;
 
@@ -126,9 +131,8 @@ public class NetworkUtils {
 
     public static String readStringSafe(PacketBuffer buffer) {
         int length = buffer.readVarInt();
-        if (length > Short.MAX_VALUE) {
-            return null;
-        }
+        if (length > Short.MAX_VALUE) return null;
+        if (length == 0) return StringUtils.EMPTY;
         String s = buffer.toString(buffer.readerIndex(), length, StandardCharsets.UTF_8);
         buffer.readerIndex(buffer.readerIndex() + length);
         return s;

src/main/java/com/cleanroommc/modularui/network/packets/OpenGuiPacket.java
@@ -19,28 +19,28 @@ import java.io.IOException;
 
 public class OpenGuiPacket<T extends GuiData> implements IPacket {
 
-    private int windowId;
+    private int networkId;
     private UIFactory<T> factory;
     private PacketBuffer data;
 
     public OpenGuiPacket() {}
 
-    public OpenGuiPacket(int windowId, UIFactory<T> factory, PacketBuffer data) {
-        this.windowId = windowId;
+    public OpenGuiPacket(int networkId, UIFactory<T> factory, PacketBuffer data) {
+        this.networkId = networkId;
         this.factory = factory;
         this.data = data;
     }
 
     @Override
     public void write(PacketBuffer buf) throws IOException {
-        buf.writeVarInt(this.windowId);
+        buf.writeVarInt(this.networkId);
         buf.writeString(this.factory.getFactoryName());
         NetworkUtils.writeByteBuf(buf, this.data);
     }
 
     @Override
     public void read(PacketBuffer buf) {
-        this.windowId = buf.readVarInt();
+        this.networkId = buf.readVarInt();
         this.factory = (UIFactory<T>) GuiManager.getFactory(buf.readString(32));
         this.data = NetworkUtils.readPacketBuffer(buf);
     }
@@ -48,7 +48,7 @@ public class OpenGuiPacket<T extends GuiData> implements IPacket {
     @SideOnly(Side.CLIENT)
     @Override
     public @Nullable IPacket executeClient(NetHandlerPlayClient handler) {
-        GuiManager.openFromClient(this.windowId, this.factory, this.data, Platform.getClientPlayer());
+        GuiManager.openFromClient(this.networkId, this.factory, this.data, Platform.getClientPlayer());
         return null;
     }
 

src/main/java/com/cleanroommc/modularui/network/packets/PacketSyncHandler.java
@@ -1,32 +1,29 @@
 package com.cleanroommc.modularui.network.packets;
 
-import com.cleanroommc.modularui.ModularUI;
 import com.cleanroommc.modularui.network.IPacket;
+import com.cleanroommc.modularui.network.ModularNetwork;
 import com.cleanroommc.modularui.network.NetworkUtils;
-import com.cleanroommc.modularui.screen.ModularContainer;
-import com.cleanroommc.modularui.screen.ModularScreen;
-
-import com.cleanroommc.modularui.value.sync.ModularSyncManager;
 
 import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.inventory.Container;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.PacketBuffer;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.IOException;
-
+@ApiStatus.Internal
 public class PacketSyncHandler implements IPacket {
 
-    private String panel;
-    private String key;
-    private boolean action;
-    private PacketBuffer packet;
+    public int networkId;
+    public String panel;
+    public String key;
+    public boolean action;
+    public PacketBuffer packet;
 
     public PacketSyncHandler() {}
 
-    public PacketSyncHandler(String panel, String key, boolean action, PacketBuffer packet) {
+    public PacketSyncHandler(int networkId, String panel, String key, boolean action, PacketBuffer packet) {
+        this.networkId = networkId;
         this.panel = panel;
         this.key = key;
         this.action = action;
@@ -35,14 +32,16 @@ public class PacketSyncHandler implements IPacket {
 
     @Override
     public void write(PacketBuffer buf) {
-        NetworkUtils.writeStringSafe(buf, this.panel);
-        NetworkUtils.writeStringSafe(buf, this.key, 64, true);
+        buf.writeVarInt(this.networkId);
+        NetworkUtils.writeStringSafe(buf, this.panel, 256, true);
+        NetworkUtils.writeStringSafe(buf, this.key, 256, true);
         buf.writeBoolean(this.action);
         NetworkUtils.writeByteBuf(buf, this.packet);
     }
 
     @Override
     public void read(PacketBuffer buf) {
+        this.networkId = buf.readVarInt();
         this.panel = NetworkUtils.readStringSafe(buf);
         this.key = NetworkUtils.readStringSafe(buf);
         this.action = buf.readBoolean();
@@ -51,30 +50,13 @@ public class PacketSyncHandler implements IPacket {
 
     @Override
     public @Nullable IPacket executeClient(NetHandlerPlayClient handler) {
-        ModularScreen screen = ModularScreen.getCurrent();
-        if (screen != null) {
-            execute(screen.getSyncManager());
-        }
+        ModularNetwork.CLIENT.receivePacket(this);
         return null;
     }
 
     @Override
     public @Nullable IPacket executeServer(NetHandlerPlayServer handler) {
-        Container container = handler.player.openContainer;
-        if (container instanceof ModularContainer modularContainer) {
-            execute(modularContainer.getSyncManager());
-        }
+        ModularNetwork.SERVER.receivePacket(this);
         return null;
-    }
-
-    private void execute(ModularSyncManager syncManager) {
-        try {
-            int id = this.action ? 0 : this.packet.readVarInt();
-            syncManager.receiveWidgetUpdate(this.panel, this.key, this.action, id, this.packet);
-        } catch (IndexOutOfBoundsException e) {
-            ModularUI.LOGGER.error("Failed to read packet for sync handler {} in panel {}", this.key, this.panel);
-        } catch (IOException e) {
-            ModularUI.LOGGER.throwing(e);
-        }
     }
 }

GTNewHorizons/ModularUI2

(cherry picked from commit 33d41a943d01b361d6b5d9d15ca23b038ce078ab)

src/main/java/com/cleanroommc/modularui/network/NetworkUtils.java
@@ -12,6 +12,7 @@ import cpw.mods.fml.common.FMLCommonHandler;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
@@ -115,6 +116,10 @@ public class NetworkUtils {
             buffer.writeVarIntToBuffer(Short.MAX_VALUE + 1);
             return;
         }
+        if (string.isEmpty()) {
+            buffer.writeVarIntToBuffer(0);
+            return;
+        }
         byte[] bytesTest = string.getBytes(StandardCharsets.UTF_8);
         byte[] bytes;
 
@@ -134,9 +139,8 @@ public class NetworkUtils {
 
     public static String readStringSafe(PacketBuffer buffer) {
         int length = buffer.readVarIntFromBuffer();
-        if (length > Short.MAX_VALUE) {
-            return null;
-        }
+        if (length > Short.MAX_VALUE) return null;
+        if (length == 0) return StringUtils.EMPTY;
         String s = buffer.toString(buffer.readerIndex(), length, StandardCharsets.UTF_8);
         buffer.readerIndex(buffer.readerIndex() + length);
         return s;

src/main/java/com/cleanroommc/modularui/network/packets/OpenGuiPacket.java
@@ -19,28 +19,28 @@ import java.io.IOException;
 
 public class OpenGuiPacket<T extends GuiData> implements IPacket {
 
-    private int windowId;
+    private int networkId;
     private UIFactory<T> factory;
     private PacketBuffer data;
 
     public OpenGuiPacket() {}
 
-    public OpenGuiPacket(int windowId, UIFactory<T> factory, PacketBuffer data) {
-        this.windowId = windowId;
+    public OpenGuiPacket(int networkId, UIFactory<T> factory, PacketBuffer data) {
+        this.networkId = networkId;
         this.factory = factory;
         this.data = data;
     }
 
     @Override
     public void write(PacketBuffer buf) throws IOException {
-        buf.writeVarIntToBuffer(this.windowId);
+        buf.writeVarIntToBuffer(this.networkId);
         NetworkUtils.writeStringSafe(buf, this.factory.getFactoryName());
         NetworkUtils.writeByteBuf(buf, this.data);
     }
 
     @Override
     public void read(PacketBuffer buf) {
-        this.windowId = buf.readVarIntFromBuffer();
+        this.networkId = buf.readVarIntFromBuffer();
         this.factory = (UIFactory<T>) GuiManager.getFactory(NetworkUtils.readStringSafe(buf));
         this.data = NetworkUtils.readPacketBuffer(buf);
     }
@@ -48,7 +48,7 @@ public class OpenGuiPacket<T extends GuiData> implements IPacket {
     @SideOnly(Side.CLIENT)
     @Override
     public @Nullable IPacket executeClient(NetHandlerPlayClient handler) {
-        GuiManager.openFromClient(this.windowId, this.factory, this.data, Platform.getClientPlayer());
+        GuiManager.openFromClient(this.networkId, this.factory, this.data, Platform.getClientPlayer());
         return null;
     }
 

src/main/java/com/cleanroommc/modularui/network/packets/PacketSyncHandler.java
@@ -1,31 +1,32 @@
 package com.cleanroommc.modularui.network.packets;
 
-import com.cleanroommc.modularui.ModularUI;
 import com.cleanroommc.modularui.network.IPacket;
+import com.cleanroommc.modularui.network.ModularNetwork;
 import com.cleanroommc.modularui.network.NetworkUtils;
 import com.cleanroommc.modularui.screen.ModularContainer;
 import com.cleanroommc.modularui.screen.ModularScreen;
 import com.cleanroommc.modularui.value.sync.ModularSyncManager;
 
 import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minecraft.inventory.Container;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.PacketBuffer;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.IOException;
-
+@ApiStatus.Internal
 public class PacketSyncHandler implements IPacket {
 
-    private String panel;
-    private String key;
-    private boolean action;
-    private PacketBuffer packet;
+    public int networkId;
+    public String panel;
+    public String key;
+    public boolean action;
+    public PacketBuffer packet;
 
     public PacketSyncHandler() {}
 
-    public PacketSyncHandler(String panel, String key, boolean action, PacketBuffer packet) {
+    public PacketSyncHandler(int networkId, String panel, String key, boolean action, PacketBuffer packet) {
+        this.networkId = networkId;
         this.panel = panel;
         this.key = key;
         this.action = action;
@@ -34,7 +35,8 @@ public class PacketSyncHandler implements IPacket {
 
     @Override
     public void write(PacketBuffer buf) {
-        NetworkUtils.writeStringSafe(buf, this.panel);
+        buf.writeVarIntToBuffer(this.networkId);
+        NetworkUtils.writeStringSafe(buf, this.panel, 256, true);
         NetworkUtils.writeStringSafe(buf, this.key, 256, true);
         buf.writeBoolean(this.action);
         NetworkUtils.writeByteBuf(buf, this.packet);
@@ -42,6 +44,7 @@ public class PacketSyncHandler implements IPacket {
 
     @Override
     public void read(PacketBuffer buf) {
+        this.networkId = buf.readVarIntFromBuffer();
         this.panel = NetworkUtils.readStringSafe(buf);
         this.key = NetworkUtils.readStringSafe(buf);
         this.action = buf.readBoolean();
@@ -50,30 +53,13 @@ public class PacketSyncHandler implements IPacket {
 
     @Override
     public @Nullable IPacket executeClient(NetHandlerPlayClient handler) {
-        ModularScreen screen = ModularScreen.getCurrent();
-        if (screen != null) {
-            execute(screen.getSyncManager());
-        }
+        ModularNetwork.CLIENT.receivePacket(this);
         return null;
     }
 
     @Override
     public @Nullable IPacket executeServer(NetHandlerPlayServer handler) {
-        Container container = handler.playerEntity.openContainer;
-        if (container instanceof ModularContainer modularContainer) {
-            execute(modularContainer.getSyncManager());
-        }
+        ModularNetwork.SERVER.receivePacket(this);
         return null;
-    }
-
-    private void execute(ModularSyncManager syncManager) {
-        try {
-            int id = this.action ? 0 : this.packet.readVarIntFromBuffer();
-            syncManager.receiveWidgetUpdate(this.panel, this.key, this.action, id, this.packet);
-        } catch (IndexOutOfBoundsException e) {
-            ModularUI.LOGGER.error("Failed to read packet for sync handler {} in panel {}", this.key, this.panel);
-        } catch (IOException e) {
-            ModularUI.LOGGER.throwing(e);
-        }
     }
 }