8dc00efa9487c472a69dafd47160b2ed276adb22
danyadev <danyadev@mail.ru>
2026-02-04 17:30:56 +0300

    deduplicate getFluidForRealItem / getFluidForRealItem methods
    they were the same, except the latter one used unnecessary ItemStack copying
    
    also disabled checking IFluidContainerItem in GTUtility method because we are already checking it earlier

--- src/main/java/com/cleanroommc/modularui/value/sync/FluidSlotSyncHandler.java
+++ src/main/java/com/cleanroommc/modularui/value/sync/FluidSlotSyncHandler.java
@@ -145,7 +145,7 @@ public class FluidSlotSyncHandler extends ValueSyncHandler<FluidStack> {
         ItemStack heldItemSizedOne = heldItem.copy();
         heldItemSizedOne.stackSize = 1;
         FluidStack currentFluid = fluidTank.getFluid();
-        FluidStack heldFluid = FluidInteractions.getFluidForRealItem(heldItemSizedOne);
+        FluidStack heldFluid = FluidInteractions.getFluidForItem(heldItemSizedOne);
         if (heldFluid != null && heldFluid.amount <= 0) {
             heldFluid = null;
         }
@@ -194,7 +194,7 @@ public class FluidSlotSyncHandler extends ValueSyncHandler<FluidStack> {
             } else {
                 ItemStack heldItemSizedOne = cursorStack.copy();
                 heldItemSizedOne.stackSize = 1;
-                FluidStack heldFluid = FluidInteractions.getFluidForPhantomItem(heldItemSizedOne);
+                FluidStack heldFluid = FluidInteractions.getFluidForItem(heldItemSizedOne);
                 if ((controlsAmount || currentFluid == null) && heldFluid != null) {
                     fillPhantom(heldFluid);
                 } else {
